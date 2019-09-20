becfcea35b4c941145940a8fad8581d3f9e37159
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-09-19 17:07:51 -0700

    Create object.js

---
+++ object.js
@@ -0,0 +1,19 @@
+var human {
+  firstName:"Eddson",
+  lastName:"Jose",
+  color:"midnight blue",
+  animal:"",
+  book:"The Dark Forest",
+  movie:"The Fountain",
+  game:"Halo Reach",
+  superhero:"Superman",
+  fact:"I am made up of stardust"
+  };
+function setup () {
+  createCanvas(400, 400);
+}
+
+function draw () {
+  background(0);
+
+}
