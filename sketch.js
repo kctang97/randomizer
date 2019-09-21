e2eb7fd9541685eb2e5bce412dbdc9f82564bfff
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-09-21 16:39:05 -0700

    update

--- sketch.js
+++ sketch.js
@@ -9,6 +9,18 @@ let user {
   fact: "love gaming"
 }
 
+var human {
+  firstName:"Eddson",
+  lastName:"Jose",
+  color:"midnight blue",
+  animal:"",
+  book:"The Dark Forest",
+  movie:"The Fountain",
+  game:"Halo Reach",
+  superhero:"Superman",
+  fact:"I am made up of stardust"
+  };
+  
 function setup() {
   createCanvas(400, 400);
 
