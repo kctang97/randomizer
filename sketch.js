61617f62b7a0347fdd719514a7dc63d0b91a7856
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-09-22 16:36:12 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(5);
 }
-\
+
 function draw() {
   background(0);
   fill(random(100, 255), random(100, 255), random(100, 255));
