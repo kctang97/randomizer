a67525775a2a2318a08229591c5e56174ec2a225
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-09-22 16:27:56 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,7 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(5);
 }
 
 function draw() {
   background(0);
-  fill(random(100, 255), random(100, 255), random(100, 255));
-  textSize(32);
-  text("hello world", random(0, width), random(0, height));
 }
