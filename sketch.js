a6c91cc4fc541b78ebae39b1444a7363ac9bab6c
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-09-22 16:08:48 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,25 @@
-let user = {
-  firstname: "Kaicong",
-  lastname: "Tang",
-  color: "black",
-  animals: "dog",
-  movie: "iron man123",
-  game:"csgo",
-  superhero: "iron man",
-  fact: "love gaming"
-}
-
-var human = { 
-  firstName:"Eddson",
-  lastName:"Jose",
-  color:"midnight blue",
-  animal:"",
-  book:"The Dark Forest",
-  movie:"The Fountain",
-  game:"Halo Reach",
-  superhero:"Superman",
-  fact:"I am made up of stardust"
-  };
+// let user = {
+//   firstname: "Kaicong",
+//   lastname: "Tang",
+//   color: "black",
+//   animals: "dog",
+//   movie: "iron man123",
+//   game:"csgo",
+//   superhero: "iron man",
+//   fact: "love gaming"
+// }
+//
+// var human = {
+//   firstName:"Eddson",
+//   lastName:"Jose",
+//   color:"midnight blue",
+//   animal:"pig",
+//   book:"The Dark Forest",
+//   movie:"The Fountain",
+//   game:"Halo Reach",
+//   superhero:"Superman",
+//   fact:"I am made up of stardust"
+//   };
 
 function setup() {
   createCanvas(400, 400);
@@ -29,6 +29,4 @@ function draw() {
   background(220);
   // text("group randomizer", 200 ,200 ,400, 400);
 
-  textSize(32);
-  text(human.firstName, width/2, height/2);
 }
