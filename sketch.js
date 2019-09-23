580dafc9424429458fdd0077af24bc655cffd2dd
kctang97 <42600194+kctang97@users.noreply.github.com>
2019-09-22 22:12:17 -0700

    object user
    not sure if it is like this

--- sketch.js
+++ sketch.js
@@ -1,11 +1,27 @@
+let user = {
+  firstname: "Kaicong",
+  lastname: "Tang",
+  favColor: "black",
+  animal: "dog",
+  movie: "iron man123",
+  game:"csgo",
+  superhero: "iron man",
+  fact: "love gaming"
+};
+
+user.isAdmin = true;
+
+let key = prompt("what do you want to know about the user? (firstname, lastname, favColor, animal, movie, game, superhero,fact) ");
+alert(user[key]);
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(5);
+  frameRate(1);
 }
 
 function draw() {
   background(0);
   fill(random(100, 255), random(100, 255), random(100, 255));
-  textSize(32);
-  text("hello world", random(0, width), random(0, height));
+  textSize(40);
+  text("Welcome", random(1, width), random(1, height));
 }
